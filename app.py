b00c2c5ea673c7ca99f9810306081f345a1a9f9d
Flavio Barboza <flmbarboza@gmail.com>
2025-06-14 17:37:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ st.markdown("""
     [data-testid="stSidebar"] > div:first-child {
         display: none !important;
     }
-    
-    /* Ajusta o espaçamento do menu */
-    [data-testid="stSidebar"] {
-        padding-top: 0px !important;
-    }
 </style>
 """, unsafe_allow_html=True)
 # --- FIM do código de remoção ---
