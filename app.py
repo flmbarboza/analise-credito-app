03b0b5fc125ed2ac4a4ec47a1fe54f6dd9c367ee
Flavio Barboza <flmbarboza@gmail.com>
2025-06-14 17:45:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-# Configuração básica
-st.set_page_config(
-    page_title="Risco de Crédito",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
 # --- SOLUÇÃO DEFINITIVA PARA O MENU ---
 with st.sidebar:
     # Container vazio para "engolir" o título padrão
@@ -14,16 +7,3 @@ with st.sidebar:
     
     # Seu menu personalizado
     st.title("📚 Menu da Disciplina")  # Título visível
-    
-    # Opções de navegação
-    pagina = st.radio(
-        "Navegação:",
-        ["🏠 Home", "🚀 Teste"],
-        label_visibility="collapsed"  # Remove label desnecessário
-    )
-
-# --- LÓGICA DE REDIRECIONAMENTO ---
-if "Home" in pagina:
-    st.switch_page("pages/1_🏠_Home.py")
-elif "Teste" in pagina:
-    st.switch_page("pages/2_🚀_Teste.py")
