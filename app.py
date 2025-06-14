0b712ce0ce8d9a5af835923dd3be19a678821372
Flavio Barboza <flmbarboza@gmail.com>
2025-06-14 17:53:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+# Configuração da página
+st.set_page_config(
+    page_title="Risco de Crédito",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 # --- SIDEBAR ---
 with st.sidebar:
     st.title("📚 Menu da Disciplina")  # Título do menu
