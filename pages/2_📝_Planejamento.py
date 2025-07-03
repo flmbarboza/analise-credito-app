8f1cab488b4a5393bfd8395a373f64dd8c40394d
Flavio Barboza <flmbarboza@gmail.com>
2025-07-03 17:08:31 -0300

    Update 2_📝_Planejamento.py

--- pages/2_📝_Planejamento.py
+++ pages/2_📝_Planejamento.py
@@ -3,7 +3,7 @@ import streamlit as st
 import time
 from pathlib import Path
 
-def mostrar_texto_pausado(texto, velocidade=0.1):
+def mostrar_texto_pausado(texto, velocidade=2):
     """Exibe texto letra por letra com efeito de digitação"""
     placeholder = st.empty()
     texto_completo = ""
